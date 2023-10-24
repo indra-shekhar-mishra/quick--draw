6b44552d5dc26f6fa3e17e766591dd331b3d2378
indra-shekhar-mishra <138847325+indra-shekhar-mishra@users.noreply.github.com>
2023-10-24 15:42:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function setup() {
+    canvas =createCanvas(280,280);
+    canvas.center();
+    background("white");
+}
+function clearCanvas(){
+    background("white");
+}
+if(condition){
+    counter++
+    }
+
+    if(stroke=='black' || stroke=='red')
+    {
+        line(30,40,25,50)
+    }
